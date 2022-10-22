bfa46787027cbf8a2f9b8d2b392d51fed848b78d
qwerty <durimfetahaj@gmail.com>
2022-10-22 17:43:46 +0200

    Added ApiSlice for Subreddit

--- redditclient/src/main.jsx
+++ redditclient/src/main.jsx
@@ -1,63 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import Root, { loader as rootLoader, action as rootAction, } from './routes/root';
-import ErrorPage from './pages/error-page';
-import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
-import EditContact, { action as editAction } from "./routes/edit";
-import { action as destroyAction } from "./routes/destroy";
+  BrowserRouter
+  
+} 
+
+from "react-router-dom";
 import darkTheme from '../src/styles/styles'
 import { ThemeProvider } from 'styled-components';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import App from '../src/routes/App'
 
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    loader: rootLoader,
-    action: rootAction,
-    errorElement: <ErrorPage />,
-    // children: [
-    //   {
-    //     errorElement: <ErrorPage />,
-    //     children: [
-    //       { index: true, element: <Index /> },
-    //       {
-    //         path: "contacts/:contactId",
-    //         element: <Contact />,
-    //         loader: contactLoader,
-    //         action: contactAction,
-    //       },
-    //       {
-    //         path: "contacts/:contactId/edit",
-    //         element: <EditContact />,
-    //         loader: contactLoader,
-    //         action: editAction,
-            
-    //       },
-    //       {
-    //         path: "contacts/:contactId/destroy",
-    //         action: destroyAction,
-    //         errorElement: <div>Oops! There was an error.</div>,
-    //       },
-    //     ],
-    //   }
-    // ]
-  },
-]);
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <BrowserRouter>
+    <App />
+      </BrowserRouter>
+  </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )

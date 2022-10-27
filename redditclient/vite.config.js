568a33b638a3e37c1a5d357b93739884e921e919
qwerty <durimfetahaj@gmail.com>
2022-10-27 09:59:35 +0200

    deploy

--- redditclient/vite.config.js
+++ redditclient/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reddit-client-app',
-  plugins: [react()],
+  plugins: [react()]
 })
